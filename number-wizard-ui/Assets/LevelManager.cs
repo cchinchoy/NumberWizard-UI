f9c2d4724b158f112c18fa64c2bd8e86ad756e96
Colin Chin Choy <cchinchoy@gmail.com>
2017-04-18 21:08:28 -0400

    First Commit

---
+++ number-wizard-ui/Assets/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load Request for : " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitGame(){
+		Debug.Log ("I want to quit");
+		Application.Quit ();
+	}
+}
---
+++ number-wizard-ui/Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+
+	int nextGuess = 10;
+	public Text text;
+
+	void Start () {
+		StartGame();
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max = max + 1;
+		guess = Random.Range (min, max);
+		text.text = guess.ToString();
+	}
+
+	public void higherGuess(){
+		min = guess;
+		NextGuess();
+	}
+
+	public void lowerGuess(){
+		max = guess;
+		NextGuess();
+	}
+	void NextGuess () {
+		nextGuess = nextGuess - 1; 
+		guess = Random.Range (min, max);
+		text.text = guess.ToString();
+		print (guess);
+		print (nextGuess);
+		if (nextGuess <= 0) {
+			Application.LoadLevel ("Win");
+		} 
+	}
+}
